1e2147761f7f6047111d38906a33a0b7dcca14d7
Brandon Hobbs <brandon.hobbs1998@gmail.com>
2023-04-29 22:36:44 -0500

    Started the to do list app to learn how to scroll through a table view with multiple cells.

---
+++ ToDo List/ToDoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Brandon Hobbs on 4/29/23.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoArray = ["Learn Swift", "Build Apps", "Change The World", "Take a Vacation"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numberOfRowsInSection was just called. Returning \(toDoArray.count)")
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoArray[indexPath.row])")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
+    }
+}
